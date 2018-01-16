1306eb1b34bc90109102d446958aed24f4cbb748
kohina <1430382648@qq.com>
2018-01-16 15:26:30 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Vue-reading
+## 目录结构
+| src 代码
+| - core 核心
+| -- index.js 入口文件 
--- src/core/index.js
+++ src/core/index.js
@@ -2,8 +2,9 @@ import Vue from './instance/index'
 import { initGlobalAPI } from './global-api/index'
 import { isServerRendering } from 'core/util/env'
 
-initGlobalAPI(Vue)
+initGlobalAPI(Vue) // 初始化
 
+// 设置服务器端渲染
 Object.defineProperty(Vue.prototype, '$isServer', {
   get: isServerRendering
 })
